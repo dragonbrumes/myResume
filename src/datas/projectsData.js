fb1ec561e5f22d2065dabb55d11479bd3d117a69
Stéphane aal <36472404+dragonbrumes@users.noreply.github.com>
2018-11-13 11:26:40 +0100

    whois.net datas update

--- src/datas/projectsData.js
+++ src/datas/projectsData.js
@@ -13,21 +13,23 @@ const projectsData = [
     // spanTime: "2018",
     jobTitle: "malta-weather.net",
     inCharge:
-      "Give current and forcecast weather via retrieving datas from an API. Made with ReactJS and NodeJS. Versioned via GitHub. Hosted on gandi.net. Librairies used : express, node-fetch, Axios, Lodash and Moment. Link: malta-weather.net"
+      "Give current and forcecast weather via retrieving datas from an API. Made with ReactJS and NodeJS. Versioned. Librairies used : express, node-fetch, Axios, Lodash and Moment. Link: malta-weather.net"
   },
   {
     // company: "ISCOM Paris",
     logo: "whoiswhois",
     // spanTime: "1999",
     jobTitle: "whois-whois.net",
-    inCharge: "Checking if a domaine name is available via an API. Made with ReactJS. Versioned via GitHub. Hosted on Netlify. Librairies used : React-Redux, Axios, SemanticUI and Stylus. whois-whois.net"
+    inCharge:
+      "Checking if a domaine name is available via an API. Made with NodeJS & ReactJS. Versioned. Librairies used : Helmet, Cors, Express, Express-rate-limit, Axios, SemanticUI : www.whois-whois.net"
   },
   {
     // company: "http://mobile-labs.eu/html/index.html",
     logo: "memorygame",
     // spanTime: "1997",
     jobTitle: "Memory Game",
-    inCharge: "Beat the machine with this memory game made with JQuery. http://mobile-labs.eu/html/index.html"
+    inCharge:
+      "Beat the machine with this memory game made with JQuery. http://mobile-labs.eu/html/index.html"
   }
 ]
 
